fd9c499a81af3fc4a212d94184e3f4b1bc9ad594
Artxe <balgoon24@gmail.com>
2026-01-27 15:00:34 +0900

    1

--- docs/js/constants.js
+++ docs/js/constants.js
@@ -1,7 +1,7 @@
 
 export default /** @type {const} */({
 	audio: { volume: .5 },
-	crosshair: { height: 50, width: 50 },
+	crosshair: { height: 14, width: 14 },
 	grid: {
 		major_every: 9,
 		size: 80,
